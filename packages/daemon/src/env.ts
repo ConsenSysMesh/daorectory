9fd16875e9efeedae9669bda37cb390b2a882c04
Vlad Kutsenko <vladdie@gmail.com>
2021-09-23 16:59:19 -0400

    fixup! fixup! fixing loading of .env vars

--- packages/daemon/src/env.ts
+++ packages/daemon/src/env.ts
@@ -1,8 +1,6 @@
 import path from 'path';
 import { config } from 'dotenv';
 
-const dotenvSettings = config({
+config({
   path: path.resolve('./.env'),
-}).parsed;
-// spread .env overwrites onto process.env to be accessed conveniently everywhere else
-process.env = Object.assign(process.env, dotenvSettings);
+})
