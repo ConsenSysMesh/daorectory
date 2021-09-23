5325aa5051232fdb517514473cd68b59ac911387
Vlad Kutsenko <vladdie@gmail.com>
2021-09-23 18:35:51 -0400

    added in-mem veramo agent and created the first daemon did

--- packages/daemon/src/index.ts
+++ packages/daemon/src/index.ts
@@ -1,13 +1,26 @@
 import './env';
-import discordClient from './discord/discord-client';
+// import discordClient from './discord/discord-client';
+import { init, agent } from './veramo/setup'
 
 console.log('Starting Daemon...');
 
-discordClient.on('interactionCreate', async (interaction) => {
-  if (interaction.isCommand()) {
-    await interaction.reply({
-      content: 'You have commanded me',
-      ephemeral: true,
-    });
-  }
-});
+// TODO: commented out discord stuff for now since it was blowing up on start
+// discordClient.on('interactionCreate', async (interaction) => {
+//   if (interaction.isCommand()) {
+//     await interaction.reply({
+//       content: 'You have commanded me',
+//       ephemeral: true,
+//     });
+//   }
+// });
+
+const main = async () => {
+  await init();
+  const identifiers = await agent.didManagerFind()
+  console.log(`There are ${identifiers.length} identifiers`)
+  identifiers.forEach((id) => {
+    console.log(id);
+  });
+}
+
+main().catch(console.error)
