c66755fa4f70570b1a4e456a21cc386809dbc390
Vlad Kutsenko <vladdie@gmail.com>
2021-10-07 16:57:25 -0400

    fixup! made .message optional for SecondedKudos

--- packages/daemon/src/veramo/veramo-types.ts
+++ packages/daemon/src/veramo/veramo-types.ts
@@ -25,13 +25,14 @@ export declare type PunkProfileVcSubject = {
 
 export declare type KudosVcSubject = {
   credentialId?: string, // unique ID we generate for this VC
-  message?: string, // optional msg to go with +1 sentiment
+  message: string,
   description: string,
   daoId?: string, // for ease of relating VCs received within a specific DAO
 };
 
 export declare type SecondedKudosVcSubject = {
   credentialId?: string,  // unique ID we generate for this VC
+  message?: string, // optional msg to go with +1 sentiment
   originalKudosId: string, // reference to credentialId of the original kudos
   daoId?: string, // for ease of relating VCs received within a specific DAO
 };
