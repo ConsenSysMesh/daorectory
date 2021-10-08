99aa81683fc1268a2e0f8b07a40ba50436da7bbb
nathan-knight <knathanknight@gmail.com>
2021-10-08 15:39:32 -0400

    Hooked up some of the punk profile information

--- packages/d-space/src/components/profile/ProfileContainer.tsx
+++ packages/d-space/src/components/profile/ProfileContainer.tsx
@@ -5,41 +5,53 @@ import ProfileTimeline from "./ProfileTimeline";
 import ProfileLink from "./ProfileLink";
 import ProfileSection from "./ProfileSection";
 import ProfileWork from "./ProfileWork";
+import type { DaemonVc, DaoProfileVc, PunkProfileVc } from '@sobol/daemon-types/veramo-types';
+import { Objects } from '../../config/constants';
 
-type Props = {}
+type Props = {
+  profile: PunkProfileVc | DaoProfileVc;
+  type: string;
+}
 
-const ProfileContainer: FC<Props> = () => (
-  <div className="ProfileContainer">
-    <ProfileSection className="ProfileContainer--header">
-      <div className="ProfileContainer--avatar">
-        <Avatar
-          src={<Image src="https://lh3.googleusercontent.com/GD12XpZ4TUHe3tgStlzewrwm27j40AVdv8vvZbCmWfTFzLBHvIsGSVfNYSz0_kKWVg3aDNQAbF2pTuLEFWUus2EZrA7eLiZYAB_kAA=s0" />}
-          size={150}
-        />
-        <h1>Funk Punk</h1>
-      </div>
-      <div className="ProfileContainer--actions">
-        <Button type="primary" icon={<ShareAltOutlined />} size="large">Share</Button>
-        <Button shape="circle" icon={<EllipsisOutlined />} size="large" />
-      </div>
-    </ProfileSection>
-    <ProfileSection className="ProfileContainer--identities">
-      <ProfileLink title="FunkPunk#6201" size="small" />
-    </ProfileSection>
-    <ProfileSection spaced>
-      <h2>About Me</h2>
-      <p>Nullam placerat ex eu orci euismod, a vulputate risus ornare. Nulla sed finibus odio. Donec elementum nulla quam, non aliquet odio efficitur ut. Curabitur et eleifend leo. Quisque ut turpis sit amet purus bibendum fermentum. Proin nulla magna, eleifend eu congue sed, sollicitudin ac felis. Praesent sed ornare leo. Donec vehicula, odio id molestie congue, ante quam posuere dolor, sit amet varius odio purus quis nisi.</p>
-    </ProfileSection>
-    <ProfileSection spaced>
-      <h2>Work Experience</h2>
-      <ProfileWork />
-    </ProfileSection>
-    <hr />
-    <ProfileSection spaced>
-      <h2>Timeline</h2>
-      <ProfileTimeline />
-    </ProfileSection>
-  </div>
-);
+const isProfilePunk = (profile: DaemonVc, type: string): profile is PunkProfileVc => type === Objects.Punk;
+
+const ProfileContainer: FC<Props> = ({ profile, type }) => {
+  const isPunk = isProfilePunk(profile, type);
+  return (
+    <div className="ProfileContainer">
+      <ProfileSection className="ProfileContainer--header">
+        <div className="ProfileContainer--avatar">
+          <Avatar
+            src={<Image src={profile.credentialSubject.avatarUrl} />}
+            size={150}
+          />
+          <h1>{profile.credentialSubject.name}</h1>
+        </div>
+        <div className="ProfileContainer--actions">
+          <Button type="primary" icon={<ShareAltOutlined />} size="large">Share</Button>
+          <Button shape="circle" icon={<EllipsisOutlined />} size="large" />
+        </div>
+      </ProfileSection>
+      <ProfileSection className="ProfileContainer--identities">
+        { isPunk && (
+          <ProfileLink title={profile.credentialSubject.handle} size="small" />
+        )}
+      </ProfileSection>
+      <ProfileSection spaced>
+        <h2>About Me</h2>
+        <p>Nullam placerat ex eu orci euismod, a vulputate risus ornare. Nulla sed finibus odio. Donec elementum nulla quam, non aliquet odio efficitur ut. Curabitur et eleifend leo. Quisque ut turpis sit amet purus bibendum fermentum. Proin nulla magna, eleifend eu congue sed, sollicitudin ac felis. Praesent sed ornare leo. Donec vehicula, odio id molestie congue, ante quam posuere dolor, sit amet varius odio purus quis nisi.</p>
+      </ProfileSection>
+      <ProfileSection spaced>
+        <h2>Work Experience</h2>
+        <ProfileWork />
+      </ProfileSection>
+      <hr />
+      <ProfileSection spaced>
+        <h2>Timeline</h2>
+        <ProfileTimeline />
+      </ProfileSection>
+    </div>
+  );
+}
 
 export default ProfileContainer;
--- packages/d-space/src/routes/Profile.tsx
+++ packages/d-space/src/routes/Profile.tsx
@@ -1,15 +1,33 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import ProfileContainer from '../components/profile/ProfileContainer';
 import { DaoProfileVc, PunkProfileVc } from '@sobol/daemon-types/veramo-types';
+import { Objects } from '../config/constants';
+import ApiClient from '../ApiClient';
+import { useRouteMatch } from 'react-router-dom';
 
 type ProfileProps = {
   type: string,
 }
 
+type RouteParams = {
+  id: string;
+}
+
 const Profile: FunctionComponent<ProfileProps> = ({ type }) => {
+  const match = useRouteMatch<RouteParams>('/*/:id');
+  if (!match) throw 'NO';
+  const { params: { id } } = match;
   const [profile, setProfile] = useState<DaoProfileVc | PunkProfileVc | null>(null);
+  useEffect(() => {
+    if (type === Objects.Punk) {
+      ApiClient.Vcs.getPunk(id)
+        .then(profile => setProfile(profile))
+    }
+  }, [type, id]);
+
+  if (!profile) return <div>loading</div>;
   return (
-    <ProfileContainer />
+    <ProfileContainer profile={profile} type={type} />
   );
 }
 
