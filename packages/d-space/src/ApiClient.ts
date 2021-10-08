28265d24f2d3d0d07bb756f68e171ae7faf04c0b
nathan-knight <knathanknight@gmail.com>
2021-10-08 14:50:40 -0400

    Added getPunk and getPunkKudos API endpoints

--- packages/d-space/src/ApiClient.ts
+++ packages/d-space/src/ApiClient.ts
@@ -1,7 +1,8 @@
-import { DaoProfileVc, PunkProfileVc } from '@sobol/daemon-types/veramo-types';
+import type { DaoProfileVc, PunkProfileVc } from '@sobol/daemon-types/veramo-types';
+import { KudosVc, SecondedKudosVc } from '@sobol/daemon-types/veramo-types';
 
 const ApiClient = {
-  dids: {
+  Vcs: {
     getDaos: async () => {
       const response = await fetch('http://localhost:8081/vcs/daos');
       const result = await response.json();
@@ -11,6 +12,16 @@ const ApiClient = {
       const response = await fetch('http://localhost:8081/vcs/punks');
       const result = await response.json();
       return result as PunkProfileVc[];
+    },
+    getPunk: async (punkId: string) => {
+      const response = await fetch(`http://localhost:8081/vcs/punks/${punkId}`);
+      const result = await response.json();
+      return result as PunkProfileVc;
+    },
+    getPunkKudos: async (punkId: string) => {
+      const response = await fetch(`http://localhost:8081/vcs/punks/${punkId}/kudos`);
+      const { kudos, secondedKudos } = await response.json();
+      return { kudos, secondedKudos } as { kudos: KudosVc[], secondedKudos: SecondedKudosVc[] };
     },
   }
 };
--- packages/daemon/src/api/vcs.router.ts
+++ packages/daemon/src/api/vcs.router.ts
@@ -1,5 +1,11 @@
 import express from 'express';
-import { getAllDaoVcs, getAllPunkVcs, getAllVcs } from '../veramo/did-manager';
+import {
+  findVcsForPunk,
+  getAllDaoVcs,
+  getAllPunkVcs,
+  getAllVcs,
+  VcTypes,
+} from '../veramo/did-manager';
 
 const router = express.Router();
 
@@ -23,11 +29,58 @@ router.get('/daos', async (req, res, next) => {
   }
 });
 
+router.get('/daos/:daoId', async (req, res, next) => {
+  try {
+    const { daoId } = req.params;
+    const vcs = await findVcsForPunk(daoId, VcTypes.DaoProfile);
+    if (vcs.length === 0) {
+      return res.status(404).json({ error: { message: 'DAO not found' } });
+    }
+    const [daoProfile] = vcs;
+    // dropping the envelope object that VCs are wrapped in for convenience
+    res.json(daoProfile.verifiableCredential);
+  } catch (e) {
+    next(e);
+  }
+});
+
 router.get('/punks', async (req, res, next) => {
   try {
     const vcs = await getAllPunkVcs();
     // dropping the envelope object that VCs are wrapped in for convenience
     res.json(vcs.map(vc => vc.verifiableCredential));
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get('/punks/:punkId', async (req, res, next) => {
+  try {
+    const { punkId } = req.params;
+    const vcs = await findVcsForPunk(punkId, VcTypes.PunkProfile);
+    if (vcs.length === 0) {
+      return res.status(404).json({ error: { message: 'Punk not found' } });
+    }
+    const [punkProfile] = vcs;
+    // dropping the envelope object that VCs are wrapped in for convenience
+    res.json(punkProfile.verifiableCredential);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get('/punks/:punkId/kudos', async (req, res, next) => {
+  try {
+    const { punkId } = req.params;
+    const [kudos, secondedKudos] = await Promise.all([
+      findVcsForPunk(punkId, VcTypes.Kudos),
+      findVcsForPunk(punkId, VcTypes.SecondedKudos),
+    ]);
+    // dropping the envelope object that VCs are wrapped in for convenience
+    res.json({
+      kudos: kudos.map(k => k.verifiableCredential),
+      secondedKudos: secondedKudos.map(k => k.verifiableCredential),
+    });
   } catch (e) {
     next(e);
   }
