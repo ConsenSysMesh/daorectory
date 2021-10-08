4ab348b2265fec726cbe80ffe354d4db3f110dca
nathan-knight <knathanknight@gmail.com>
2021-10-08 15:19:55 -0400

    Scrollbar doesn't go under the header anymore

--- packages/d-space/src/App.tsx
+++ packages/d-space/src/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import Router from "./routes/Router";
-import { Layout } from "antd";
 import Header from "./layouts/Header";
 import Background from "./layouts/Background";
 import Content from "./layouts/Content";
 
 const App = () => (
-  <Layout className="App">
+  <div className="App">
     <Background />
     <Header />
     <Content className="App--content">
       <Router />
     </Content>
-  </Layout>
+  </div>
 );
 
 export default App;
